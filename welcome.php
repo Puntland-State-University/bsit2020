147735b6634d76fcb646a2b647a786e5a2c151b3
MustafaWIthA <mustafa.abdsh@gmail.com>
2022-10-03 23:06:47 +0300

    Initial commit

---
+++ welcome.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>welcome</title>
+</head>
+
+<body>
+
+    <main>
+        <?php
+        echo "ASC ku soo dhawoow PHP iyo mysql iyo kuwo kale";
+        ?>
+    </main>
+
+</body>
+
+</html>
